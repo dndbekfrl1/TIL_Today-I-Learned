7b68f03f0f4d08f2fafd62814f250818b99afc4e
dndbekfrl1 <dndbekfrl1@gmail.com>
2021-07-16 02:15:10 +0900

    refactoring with useReducer

--- react/test/src/App.js
+++ react/test/src/App.js
@@ -1,13 +1,23 @@
 import "./App.css";
-import React, { useRef, useState, useMemo, useCallback } from "react";
+import React, {
+  useRef,
+  useState,
+  useMemo,
+  useCallback,
+  useReducer,
+} from "react";
 import UserList from "./UserList";
 import CreateUser from "./CreateUser";
 
 function countActiveUsers(users) {
   return users.filter((user) => user.active).length;
 }
-function App() {
-  const [users, setUsers] = useState([
+const initialState = {
+  inputs: {
+    username: "",
+    email: "",
+  },
+  users: [
     {
       id: 1,
       username: "velopert",
@@ -26,52 +36,81 @@ function App() {
       email: "liz@example.com",
       active: false,
     },
-  ]);
+  ],
+};
+function reducer(state, action) {
+  console.log("what is action?", action);
+  switch (action.type) {
+    case "CHANGE_INPUT":
+      return {
+        ...state,
+        inputs: {
+          ...state.inputs,
+          [action.name]: action.value,
+        },
+      };
+    case "CREATE_USER":
+      return {
+        inputs: initialState.inputs,
+        users: state.users.concat(action.user),
+      };
+    case "TOGGLE_USER":
+      return {
+        ...state,
+        users: state.users.map((user) =>
+          user.id === action.id ? { ...user, active: !user.active } : user
+        ),
+      };
 
-  const [inputs, setInputs] = useState({
-    username: "",
-    email: "",
-  });
+    case "REMOVE_USER":
+      return {
+        ...state,
+        users: state.users.filter((user) => user.id !== action.id),
+      };
+    default:
+      return state;
+  }
+}
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
   const nextId = useRef(4);
-  const { username, email } = inputs;
-  //username과 email은 onChage setInputs가 호출될때마다 갱신되는건가?? -> 네 그렇습니다.
+  const { users } = state;
+  const { username, email } = state.inputs;
   const onChange = useCallback((e) => {
     const { name, value } = e.target;
-    setInputs((inputs) => ({
-      ...inputs,
-      [name]: value,
-    }));
-  }, []);
-
-  const onCreate = useCallback(() => {
-    const user = {
-      id: nextId.current,
-      username,
-      email,
-    };
-    setUsers((users) => users.concat(user));
-
-    setInputs({
-      username: "",
-      email: "",
+    console.log("name:", name, "value", value);
+    dispatch({
+      type: "CHANGE_INPUT",
+      name,
+      value,
     });
-
+  }, []);
+  const onCreate = useCallback(() => {
+    dispatch({
+      type: "CREATE_USER",
+      user: {
+        id: nextId.current,
+        username,
+        email,
+      },
+    });
     nextId.current += 1;
-    console.log("oncreate usecallback", username);
-  }, [username, email]); //users, username, email 셋 다 바뀔때 호출되는건가?-> 그건아닌데 갱신에 문제가 생김. deps[]에서 값을 참조해야 정상적인 작동을 함 [users]일때, id값만 갱신 [users,username]일 때, users, username 만
+  }, [username, email]);
+
+  const onToggle = useCallback((id) => {
+    dispatch({
+      type: "TOGGLE_USER",
+      id,
+    });
+  }, []);
 
   const onRemove = useCallback((id) => {
-    setUsers((users) => users.filter((user) => user.id !== id));
-  }, []);
-  const onToggle = useCallback((id) => {
-    setUsers((users) =>
-      users.map((user) =>
-        user.id === id ? { ...user, active: !user.active } : user
-      )
-    );
-  }, []);
+    dispatch({
+      type: "REMOVE_USER",
+      id,
+    });
+  });
 
-  const count = useMemo(() => countActiveUsers(users), [users]);
   return (
     <>
       <CreateUser
@@ -80,40 +119,9 @@ function App() {
         onChange={onChange}
         onCreate={onCreate}
       />
-      <UserList
-        users={users}
-        onRemove={onRemove}
-        onToggle={onToggle}
-      ></UserList>
-      <div>활성사용자 수 :{count}</div>
+      <UserList users={users} onToggle={onToggle} onRemove={onRemove} />
+      <div>활성자 수 :0</div>
     </>
   );
 }
 export default App;
-
-// class Clock extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = { date: new Date() };
-//   }
-//   render() {
-//     return (
-//       <div>
-//         <h1>Hello, world!</h1>
-//         <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-//       </div>
-//     );
-//   }
-// }
-
-// function tick() {
-//   ReactDOM.render(<Clock />, document.getElementById("root"));
-// }
-
-// setInterval(tick, 1000);
-
-// class App extends React.Component {
-//   render() {
-//     return <div>hello</div>;
-//   }
-// }
