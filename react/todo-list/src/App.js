223c2eb2af1d0ed46925de59aad193d68d9cb0a7
dndbekfrl1 <dndbekfrl1@gmail.com>
2021-07-21 17:50:29 +0900

    todolist template

---
+++ react/todo-list/src/App.js
@@ -0,0 +1,26 @@
+import "./App.css";
+import { createGlobalStyle } from "styled-components";
+import TodoTemplate from "./components/TodoTemplate";
+import TodoHead from "./components/TodoHead";
+import TodoList from "./components/TodoList";
+import TodoCreate from "./components/TodoCreate";
+
+const GlobalStyle = createGlobalStyle`
+body{
+  background:#e9ecef;
+}`;
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <TodoTemplate>
+        <TodoHead></TodoHead>
+        <TodoList></TodoList>
+        <TodoCreate />
+      </TodoTemplate>
+    </>
+  );
+}
+
+export default App;
