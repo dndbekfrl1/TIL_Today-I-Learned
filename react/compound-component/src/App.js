0bcebd9e9eaa3587facc29d4b1532dceb55a6bf4
dndbekfrl1 <dndbekfrl1@gmail.com>
2021-12-13 22:05:03 +0900

    learn react compound component

--- react/compound-component/src/App.js
+++ react/compound-component/src/App.js
@@ -1,8 +1,41 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Card from "./Card";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {/* First example from the tutorial */}
+      <Card>
+        <Card.Heading>My title</Card.Heading>
+        <Card.Button>Toggle</Card.Button>
+      </Card>
+      {/* Example with button and heading flipped */}
+      <Card>
+        <Card.Button>Toggle</Card.Button>
+        <Card.Heading>My title</Card.Heading>
+      </Card>
+      {/* Example with image */}
+      <Card>
+        <Card.Heading>My title</Card.Heading>
+        <Card.Image
+          src="https://picsum.photos/300/100?random=0"
+          alt="Our trip to the beach"
+        />
+        <Card.Button>Toggle</Card.Button>
+      </Card>
+      {/* Example with an avatar-image (type="avatar") */}
+      <Card>
+        <Card.Image
+          src="https://picsum.photos/48?random=1"
+          alt="This is me"
+          type="avatar"
+        />
+        <Card.Heading>My title</Card.Heading>
+        <Card.Button>Toggle</Card.Button>
+      </Card>
+    </div>
+  );
 }
 
 export default App;
--- react/compound-component/src/Card.js
+++ react/compound-component/src/Card.js
@@ -1,9 +1,19 @@
 import { createContext, useContext, useState } from "react";
-
+import "./Card.css";
 /**
  * scope를 적용해보자.
  * scope를 한다면 Heading은 Card 컴포넌트 안에서만 사용할 수 있다.
  * -> context api를 사용하자
+ * -> 커스텀 훅으로 더 발전시키자!
+ *
+ * compound-component의 장점
+ * flexible하고 expandable함!
+ * 협업하기 쉬워짐!
+ *
+ * flexibility:
+ * Card컴포넌트에 버튼과 헤딩 컴포넌트의 위치를 자유자재로 할 수 있고
+ * define props/attributes to each component freely할 수 있다.
+ *
  */
 
 function useCardContext() {
@@ -22,32 +32,50 @@ function Card({ children }) {
   let [toggled, setToggled] = useState(false);
   return (
     <CardContext.Provider value={{ toggled, setToggled }}>
-      <div className="Card">
-        <Card.Heading>My title</Card.Heading>
-        {/* Heading은 이제 Card에 belongs to된다! 이것이 compound component입니다.
-         */}
-        <Card.Button>Toggle</Card.Button>
+      <div className={toggled ? "Card Card--highlight" : "Card"}>
+        {children}
       </div>
     </CardContext.Provider>
   );
 }
 
 function Heading({ children }) {
-  let context = useCardContext(); //이 context는 CardContext.Provider의 value에 접근할 수 있다!
+  let { toggled } = useCardContext(); //이 context는 CardContext.Provider의 value에 접근할 수 있다!
   //이렇게 사용하면 Card.Heading은 Card밖에서 사용시 context값은 undefined가 되는 장점이 있다.
 
-  if (!context) {
-    return <p className="Card_heading">Card 컴포넌트 안에 있지 않아요</p>;
-  }
-  return <h2 className="Card__heading">{children}</h2>;
+  return (
+    <h2
+      className={
+        toggled ? "Card__heading Card__heading--highlight" : "Card__heading"
+      }
+    >
+      {children}
+    </h2>
+  );
 }
 
 function Button({ children }) {
-  let context = useCardContext();
+  let { setToggled } = useCardContext();
 
-  return <button className="Card_button">{children}</button>;
+  return (
+    <button className="Card_button" onClick={() => setToggled((prev) => !prev)}>
+      {children}
+    </button>
+  );
+}
+
+function Image({ src, alt, type }) {
+  useCardContext();
+  return (
+    <img
+      className={`Card_image${type ? " Card_image--" + type : ""}`}
+      src={src}
+      alt={alt}
+    />
+  );
 }
 
 Card.Heading = Heading; // Heading은 Card 오브젝트의 메소드가 됨
 Card.Button = Button;
+Card.Image = Image;
 export default Card;
